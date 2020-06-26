8aab85853f516d22987900be9417aff127bd0d60
matancarmeli7 <matan.carmeli7@gmail.com>
2020-06-26 14:44:24 +0300

    Added main function and create_arguments

--- runner.py
+++ runner.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
 import argparse
 
-parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
-    description='Python script that wraps any other command and outputs a summary of execution')
-parser.add_argument('-c', metavar='Count', default=1, type=int, nargs=1,
-                    help= 'Number of times to run the given command',)
-parser.add_argument('--failed-count', metavar='N', type=int, nargs=1,
-                    help= 'Number of allowed failed command invocation attempts before givingup')
-parser.add_argument('--sys-trace', metavar='',
-                    help= 'Creates a log for each of the following values:\n'
-                    '  * Disk IO\n  * Memory\n  * Process & CPU usage of the command\n  * Network information')
-parser.add_argument('--call-trace', metavar='',
-                    help= 'For each failed execution, add to a log with all the system calls that ran by the command')
-parser.add_argument('--log-trace', metavar='',
-                    help= 'For each failed execution, add to a log the stdout and the stdin of the command')
-parser.add_argument('--debug', metavar='',
-                    help= 'Debug mode')
+# Defines what arguments the runner.py can get
+def create_arguments():
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
+        description='Python script that wraps any other command and outputs a summary of execution')
+    parser.add_argument('-c', metavar='Count', default=1, type=int,
+                        help= 'Number of times to run the given command (default: 1)',)
+    parser.add_argument('--failed-count', metavar='N', type=int, default=0,
+                        help= 'Number of allowed failed command invocation attempts before givingup (default: 1)')
+    parser.add_argument('--sys-trace', metavar='',
+                        help= 'Creates a log for each of the following values:\n'
+                              '  * Disk IO\n  * Memory\n  * Process & CPU usage of the command\n  * Network information')
+    parser.add_argument('--call-trace', metavar='',
+                        help= 'For each failed execution, add to a log with all the system calls that ran by the command')
+    parser.add_argument('--log-trace', metavar='',
+                        help= 'For each failed execution, add to a log the stdout and the stdin of the command')
+    parser.add_argument('--debug', metavar='',
+                        help= 'Debug mode')
+    args = parser.parse_args()
 
-args = parser.parse_args()
-command = input("Enter your command: ")
+    if args.failed_count > args.c:
+        parser.error('--failed-count value must be equal or lower than -c value')
+
+    return args
+
+if __name__ == "__main__":
+    args = create_arguments()
+    command = input("Enter your command: ")

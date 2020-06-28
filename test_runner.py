9137be06309a4dc8a91e3cc943b176a65d1d805e
matancarmeli7 <matan.carmeli7@gmail.com>
2020-06-28 22:53:09 +0300

    first test test_should_run_cmd_n_times_succefully

--- test_runner.py
+++ test_runner.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from subprocess import PIPE, run
+from datetime import datetime
 import pytest
 import runner
 
@@ -7,6 +8,3 @@ def test_should_run_cmd_n_times_succefully():
     total_return_codes = runner.create_runner(
         'ls -l', 3, 0, False, False, False, False)    
     assert total_return_codes == 3
-
-def test_should_create_log_file():
-    

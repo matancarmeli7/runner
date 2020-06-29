85649a91a1470dc15c2fa63e1e39f35daa715433
Matan Tarif <purim@hayom.com>
2020-06-29 20:25:27 +0300

    Removed unnecessary False statment while calling create_runner function

--- test_runner.py
+++ test_runner.py
@@ -8,13 +8,13 @@ import runner
 
 def test_should_run_cmd_n_times_succefully():
     total_return_codes = runner.create_runner(
-        'ls -l', 3, 0, False, False, False, False, False)
+        'ls -l', 3, 0)
     assert total_return_codes == 3
 
 
 def test_should_run_cmd_n_times_not_succefully():
     total_return_codes = runner.create_runner(
-        'bad_command', 3, 2, False, False, False, False, False)
+        'bad_command', 3, 2)
     assert total_return_codes == 2
 
 
@@ -38,7 +38,7 @@ def test_should_create_log_file_and_check_if_sys_trace_option_works():
     correct_log_file = '{}'.format(datetime.now().strftime(
         'runner_number_1_date_%H_%M_%d_%m_%Y.log'))
     runner.create_runner(
-        'ls -l some_not_existing_file', 1, 0, True, False, False, False, False)
+        'ls -l some_not_existing_file', 1, 0, True)
     test_command_output = run('grep cpu {0}; grep disk {0}; grep network {0}; grep memory {0}'
                               .format(correct_log_file), shell=True)
     assert test_command_output.returncode == 0
@@ -48,7 +48,7 @@ def test_should_create_log_file_and_check_if_call_trace_option_works():
     correct_log_file = '{}'.format(datetime.now().strftime(
         'runner_number_1_date_%H_%M_%d_%m_%Y.log'))
     runner.create_runner(
-        'ls -l some_not_existing_file', 1, 0, False, True, False, False, False)
+        'ls -l some_not_existing_file', 1, 0, False, True)
     test_command_output = run('grep "system calls" {}'
                               .format(correct_log_file), shell=True)
     assert test_command_output.returncode == 0
@@ -58,7 +58,7 @@ def test_should_create_log_file_and_check_if_log_trace_option_works():
     correct_log_file = '{}'.format(datetime.now().strftime(
         'runner_number_1_date_%H_%M_%d_%m_%Y.log'))
     runner.create_runner(
-        'ls -l some_not_existing_file', 1, 0, False, False, True, False, False)
+        'ls -l some_not_existing_file', 1, 0, False, False, True)
     test_command_output = run('grep stdout {}'
                               .format(correct_log_file), shell=True)
     assert test_command_output.returncode == 0
@@ -68,7 +68,7 @@ def test_should_create_pcap_file_and_check_if_net_trace_option_works():
     correct_pcap_file = '{}'.format(datetime.now().strftime(
         'runner_number_0_date_%H_%M_%d_%m_%Y.pcap'))
     runner.create_runner(
-        'sleep 2; ls -l mm', 1, 0, False, False, False, True, False)
+        'sleep 2; ls -l mm', 1, 0, False, False, False, True)
     test_command_output = run('tcpdump -r {}'
                               .format(correct_pcap_file), shell=True)
     assert test_command_output.returncode == 0

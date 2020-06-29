bc4fb32567b01458f1cb897c34674b7bf46d50cb
Matan Tarif <purim@hayom.com>
2020-06-29 15:19:42 +0300

    Add test function test_should_create_pcap_file_and_check_if_net_trace_option_works

--- test_runner.py
+++ test_runner.py
@@ -39,7 +39,7 @@ def test_should_create_log_file_and_check_if_call_trace_option_works():
     correct_log_file = '{}'.format(datetime.now().strftime('/home/matan/runner_number_1_date_%H_%M_%d_%m_%Y.log'))
     runner.create_runner(
         'ls -l some_not_existing_file', 1, 0, False, True, False, False)
-    test_command_output = run('grep "system calls" {0}'\
+    test_command_output = run('grep "system calls" {}'\
                               .format(correct_log_file), shell=True)
     assert test_command_output.returncode == 0
     
@@ -47,6 +47,14 @@ def test_should_create_log_file_and_check_if_log_trace_option_works():
     correct_log_file = '{}'.format(datetime.now().strftime('/home/matan/runner_number_1_date_%H_%M_%d_%m_%Y.log'))
     runner.create_runner(
         'ls -l some_not_existing_file', 1, 0, False, False, True, False)
-    test_command_output = run('grep stdout {0}'\
+    test_command_output = run('grep stdout {}'\
                               .format(correct_log_file), shell=True)
+    assert test_command_output.returncode == 0
+
+def test_should_create_pcap_file_and_check_if_net_trace_option_works():
+    correct_pcap_file = '{}'.format(datetime.now().strftime('/home/matan/runner_number_0_date_%H_%M_%d_%m_%Y.pcap'))
+    runner.create_runner(
+        'sleep 2; ls -l mm', 1, 0, False, False, False, True)
+    test_command_output = run('tcpdump -r {}'\
+                              .format(correct_pcap_file), shell=True)
     assert test_command_output.returncode == 0

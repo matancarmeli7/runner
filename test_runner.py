41895a651bc3880ca90afacfe8f879cbdce36ec7
matancarmeli7 <matan.carmeli7@gmail.com>
2020-06-29 10:10:00 +0300

    Add test function test_should_create_log_file_and_check_if_sys_trace_option_works

--- test_runner.py
+++ test_runner.py
@@ -25,3 +25,11 @@ def test_should_create_pcap_file():
     pcap_file = runner.create_pcap_file(
         "runner_test")
     assert pcap_file == correct_pcap_file
+    
+def test_should_create_log_file_and_check_if_sys_trace_option_works():
+    correct_log_file = '{}'.format(datetime.now().strftime('/home/matan/runner_number_1_date_%H_%M_%d_%m_%Y.log'))
+    runner.create_runner(
+        'ls -l some_not_existing_file', 1, 0, True, False, False, False)
+    test_command_output = run('grep cpu {0}; grep disk {0}; grep network {0}; grep memory {0}'\
+                              .format(correct_log_file), shell=True)
+    assert test_command_output.returncode == 0
